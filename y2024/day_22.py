6644385b34cfb1f4221ff4a1966d1399760fc986
Robin Kåveland <kaaveland@gmail.com>
2024-12-23 09:24:47 +0100

    Replace d-variables with bit-shifting and masking k

--- y2024/day_22.py
+++ y2024/day_22.py
@@ -3,10 +3,11 @@ from collections import Counter
 
 def main(inp: str) -> str:
     p1, counter = 0, Counter()
+    k_mask = (1 << 20) - 1
     for monkey in inp.splitlines():
         secret: int = int(monkey)
         last_price = secret % 10
-        d1 = d2 = d3 = None
+        k = 0
         seen = set()
 
         for i in range(2000):
@@ -14,15 +15,8 @@ def main(inp: str) -> str:
             secret = ((secret >> 5) ^ secret) & 0xFFFFFF
             secret = ((secret << 11) ^ secret) & 0xFFFFFF
             price = secret % 10
-            (d0, d1, d2) = (d1, d2, d3)
-            d3 = price - last_price + 9  # ensure min val is 0 to construct k
+            k = ((k << 5) | (price - last_price + 9) & 0x1F) & k_mask
             if i >= 3:  # seen 4 prices
-                k = (
-                    (d0 & 0x1F)
-                    | ((d1 & 0x1F) << 5)
-                    | ((d2 & 0x1F) << 10)
-                    | ((d3 & 0x1F) << 15)
-                )
                 if k not in seen:
                     seen.add(k)
                     counter[k] += price
