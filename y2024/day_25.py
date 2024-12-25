501fb0e7ce0cb254a87fa6b7f54be0e84d844c61
Robin Kåveland <kaaveland@gmail.com>
2024-12-25 19:27:03 +0100

    Solve 2024 day 25

---
+++ y2024/day_25.py
@@ -0,0 +1,19 @@
+def main(s: str) -> str:
+    blocks = s.split("\n\n")
+    keys, locks = [], []
+    for block in blocks:
+        dest = locks if block.startswith("#") else keys
+        dest.append(
+            {
+                (x, y)
+                for y, line in enumerate(block.splitlines())
+                for x, ch in enumerate(line)
+                if ch == "#"
+            }
+        )
+    could_fit = 0
+    for key in keys:
+        for lock in locks:
+            if not key & lock:
+                could_fit += 1
+    return f"Part 1: {could_fit}, Part 2: Collect stars"
